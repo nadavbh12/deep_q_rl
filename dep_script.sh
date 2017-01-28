46fd11f7f2fef6172a3a5a17ce66709f8f48deec
Nadavbh <nadavbh@gmail.com>
2017-01-28 22:22:18 +0200

    modified dep_script

--- dep_script.sh
+++ dep_script.sh
@@ -11,17 +11,23 @@ sudo apt-get install python-opencv
 echo "==>installing Matplotlib..."
 sudo apt-get install python-matplotlib python-tk
 
+mkdir build
+cd build
 echo "==>installing Theano ..."
 # some dependencies ...
 sudo apt-get install python-numpy python-scipy python-dev python-pip python-nose g++ libopenblas-dev git
-pip install --user --upgrade --no-deps git+git://github.com/Theano/Theano.git
+# new version of Theano has incompatibility issues, therefore install from older branch
+#pip install --user --upgrade --no-deps git+git://github.com/Theano/Theano.git
+git clone --branch rel-0.8.2 https://github.com/Theano/Theano.git
+cd Theano
+pip install .
+cd ..
 
 echo "==>installing Lasagne ..."
 pip install --user --upgrade https://github.com/Lasagne/Lasagne/archive/master.zip
 
 # Packages below this point require downloads.
-mkdir build
-cd build
+mkdir cores
 
 if [ ! -d "./pylearn2" ]
 then
@@ -41,14 +47,14 @@ echo "==>installing RLE ..."
 # dependencies ...
 sudo apt-get install libsdl1.2-dev libsdl-gfx1.2-dev libsdl-image1.2-dev cmake
 
-git clone https://github.com/nadavbh12/Retro-Learning-Environment.git RLE
+git clone --branch 1.0.2 https://github.com/nadavbh12/Retro-Learning-Environment.git RLE
 mkdir cores
 cd ./RLE
 cmake -DUSE_SDL=ON -DBUILD_EXAMPLES=OFF .
 make -j 4
 pip install --user .
-ln -s stella-libretro/stella_libretro.so ../../cores/
-ln -s snes9x2010/snes9x2010_libretro.so ../../cores/
+cp stella-libretro/stella_libretro.so ../../cores/
+cp snes9x2010/snes9x2010_libretro.so ../../cores/
 cd ..
 fi
 
